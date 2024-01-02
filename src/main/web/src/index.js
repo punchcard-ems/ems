890aa0c43a41149676fdba7ebbe889747121ad39
Holden Cormier <hcormie2@villanova.edu>
2024-01-02 18:21:32 -0500

    fixing file structure

--- src/main/web/src/index.js
+++ src/main/web/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './css/index.css';
+import App from './components/App';
+import reportWebVitals from './misc/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
