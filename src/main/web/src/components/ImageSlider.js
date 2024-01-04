6909a34df6b0f20eb80dc3426b18880035cc763a
Holden Cormier <hcormie2@villanova.edu>
2024-01-03 19:44:14 -0500

    image slider works

---
+++ src/main/web/src/components/ImageSlider.js
@@ -0,0 +1,81 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+const slideStyles = {
+    width: "100%",
+    height: "100%",
+    borderRadius: "10px",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+};
+
+const sliderStyles = {
+    position: "relative",
+    height: "100%",
+};
+
+const slidesContainerStyles = {
+    display: "flex",
+    height: "100%",
+    transition: "transform ease-out 0.3s",
+};
+
+const slidesContainerOverflowStyles = {
+    overflow: "hidden",
+    height: "100%",
+};
+
+const ImageSlider = ({ slides, parentWidth }) => {
+    const timerRef = useRef(null);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const goToPrevious = () => {
+        const isFirstSlide = currentIndex === 0;
+        const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+        setCurrentIndex(newIndex);
+    };
+    const goToNext = useCallback(() => {
+        const isLastSlide = currentIndex === slides.length - 1;
+        const newIndex = isLastSlide ? 0 : currentIndex + 1;
+        setCurrentIndex(newIndex);
+    }, [currentIndex, slides]);
+    const goToSlide = (slideIndex) => {
+        setCurrentIndex(slideIndex);
+    };
+    const getSlideStylesWithBackground = (slideIndex) => ({
+        ...slideStyles,
+        backgroundImage: `url(${slides[slideIndex].url})`,
+        width: `${parentWidth}px`,
+    });
+    const getSlidesContainerStylesWithWidth = () => ({
+        ...slidesContainerStyles,
+        width: parentWidth * slides.length,
+        transform: `translateX(${-(currentIndex * parentWidth)}px)`,
+    });
+
+    useEffect(() => {
+        if (timerRef.current) {
+            clearTimeout(timerRef.current);
+        }
+        timerRef.current = setTimeout(() => {
+            goToNext();
+        }, 4000);
+
+        return () => clearTimeout(timerRef.current);
+    }, [goToNext]);
+
+    return (
+        <div style={sliderStyles}>
+            <div style={slidesContainerOverflowStyles}>
+                <div style={getSlidesContainerStylesWithWidth()}>
+                    {slides.map((_, slideIndex) => (
+                        <div
+                            key={slideIndex}
+                            style={getSlideStylesWithBackground(slideIndex)}
+                        ></div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ImageSlider;
--- src/main/web/src/components/pages/Home.js
+++ src/main/web/src/components/pages/Home.js
@@ -1,7 +1,44 @@
+import ImageSlider from "../ImageSlider";
+import React from "react";
+
 export default function Home() {
+    const slides = [
+        { url: "http://localhost:3000/punchcard-shift-creation.jpg", title: "Shift Creation" },
+        { url: "http://localhost:3000/punchcard-shift-display.jpg", title: "Shift Display" },
+        { url: "http://localhost:3000/punchcard-shift-transfer.jpg", title: "Shift Transfer" },
+    ];
+
+    const texts = [
+        { text: "Shift Creation" },
+        { text: "Shift Display" },
+        { text: "Shift Transfer" },
+    ];
+
+
+    const containerStyles = {
+        width: "500px",
+        height: "680px",
+        margin: "0 auto",
+    };
+
+    // using inline until i can figure out how to import the correct ones
+    const pageStyles = {
+      display: "flex",
+    };
+
+
     return (
-        <div className="home">
-            <h1>Home</h1>
+        <div style={pageStyles}>
+            <div>
+                <h3>Built For Employees</h3>
+                <p>
+                    Redefine how your organization handles employee management.
+                    Put your employees in the driver seat of their schedules and revolutionize your employee satisfaction
+                </p>
+            </div>
+            <div style={containerStyles}>
+                <ImageSlider slides={slides} parentWidth={500} />
+            </div>
         </div>
-    )
+    );
 }
